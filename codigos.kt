1af4a13a1acd715047b2d57c365aae0cb1fd56f1
miguelcardosoereno2012-byte <miguelcardosoereno2012@gmail.com>
2026-05-18 09:18:41 -0300

    Update codigos.kt

--- codigos.kt
+++ codigos.kt
@@ -1,1 +1,16 @@
-
+class PedidoProcessor {
+ fun processarPedido(pedido: Pedido) {
+when (pedido.status) {
+  "novo" -> {
+     println("Pedido em processamento: ${pedido.id}")
+     Thread.sleep(2000) // Simulando processamento
+      }
+   "processado" -> {
+      println("Pedido já processado: ${pedido.id}")
+       }
+    else -> {
+      println("Status desconhecido do pedido: ${pedido.id}")
+       }
+     }
+ }
+ }
